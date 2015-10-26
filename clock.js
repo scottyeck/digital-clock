d3b896d9057e397b7bec27af9533d805ac923605
Scott A Eckenthal <scott.eckenthal@gmail.com>
2015-10-25 23:07:30 -0400

    Wraps in IIFE

--- clock.js
+++ clock.js
@@ -1,12 +1,16 @@
-$(document).ready(function(){
+(function(window, $, undefined) {
 
-  // $("#hours").text()
-  setInterval(function(){
-    var dateTime = new Date();
-    var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-    $("#milliseconds").text(dateTime.getMilliseconds());
-    $("#seconds").text(dateTime.getSeconds());
-    $("#minutes").text(minutesTwoDigitsWithLeadingZero);
-    $("#hours").text(dateTime.getHours());
-  },100)
-})
+  $(document).ready(function(){
+
+    window.setInterval(function(){
+      var dateTime = new Date();
+      var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
+      $("#milliseconds").text(dateTime.getMilliseconds());
+      $("#seconds").text(dateTime.getSeconds());
+      $("#minutes").text(minutesTwoDigitsWithLeadingZero);
+      $("#hours").text(dateTime.getHours());
+    }, 100);
+
+ });
+
+}(this, this.jQuery));
