7a123ca73cedac2979f5c41406f3239492ae7138
Scott A Eckenthal <scott.eckenthal@gmail.com>
2015-10-25 23:22:15 -0400

    Slight cleanup for readability / control flow clarity

--- clock.js
+++ clock.js
@@ -2,13 +2,38 @@
 
   $(document).ready(function(){
 
+    var dateTime,
+        milliseconds,
+        seconds,
+        minutes,
+        hours;
+
+    // Cache DOM elements
+    var $milliseconds = $('#milliseconds'),
+        $seconds = $('#seconds'),
+        $minutes = $('#minutes'),
+        $hours = $('#hours');
+
     window.setInterval(function(){
-      var dateTime = new Date();
-      var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-      $("#milliseconds").text(dateTime.getMilliseconds());
-      $("#seconds").text(dateTime.getSeconds());
-      $("#minutes").text(minutesTwoDigitsWithLeadingZero);
-      $("#hours").text(dateTime.getHours());
+
+      // Get current time
+      dateTime = new Date();
+
+      // Store desired time attributes
+      milliseconds = dateTime.getMilliseconds();
+      seconds = dateTime.getSeconds();
+      minutes = dateTime.getMinutes();
+      hours = dateTime.getHours();
+
+      // Ensure minutes attribute is printed with two digits (using
+      // leading zero if necessary).
+      minutes = ("0" + minutes).substr(-2);
+
+      // Update DOM elements
+      $milliseconds.text(milliseconds);
+      $seconds.text(seconds);
+      $minutes.text(minutes);
+      $hours.text(hours);
     }, 100);
 
  });
