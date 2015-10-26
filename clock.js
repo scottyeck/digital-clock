ea4cedafdf6d93cdb7064b377e7e38e700be594f
Scott A Eckenthal <scott.eckenthal@gmail.com>
2015-10-25 23:24:07 -0400

    Adds high-level block doc

--- clock.js
+++ clock.js
@@ -1,5 +1,14 @@
 (function(window, $, undefined) {
 
+  /*
+   * This is an exercise in creating a Digital Clock using Javascript
+   * and jQuery used in NYCDA's Introductory Web Development courses.
+   *
+   * We create this clock by establishing a timed interval, during
+   * each iteration of which we get the current time and update the
+   * corresponding DOM nodes representing the "time-components" (i.e.
+   * hours, minutes, seconds, milliseconds).
+   */
   $(document).ready(function(){
 
     var dateTime,
